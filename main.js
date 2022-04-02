c093b168f60fb4a0a8c81e5659820708cb2aec1f
Suraj Rai <surajrai@Surajs-MacBook-Pro.local>
2022-04-02 16:54:56 +0530

    add: javascript to main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// RATING
+const ratingStars = document.querySelectorAll(".rating");
+
+const handleRating = (idx, ratingStars) => {
+  // for chnaging the star color to blue to the preceeding stars to the clicked star
+  for (let i = 0; i <= idx; i++) {
+    if (!ratingStars[i].classList.contains('"rating-star-active"')) {
+      ratingStars[i].classList.add("rating-star-active");
+    }
+  }
+  //   for changing the star color to default beyond the clicked star
+  for (let i = idx + 1; i < ratingStars.length; i++) {
+    ratingStars[i].classList.remove("rating-star-active");
+  }
+};
+ratingStars.forEach((star, idx) => {
+  star.addEventListener("click", () => handleRating(idx, ratingStars));
+});
