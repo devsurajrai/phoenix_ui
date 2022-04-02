89f2ecf66da751e8b870a775ec8faa75a906ba6e
Suraj Rai <surajrai@Surajs-MacBook-Pro.local>
2022-04-02 17:01:55 +0530

    add: javascript to main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import "./components/modal/modal";
+// MODAL
+const unHideModalButton = document.querySelector(".modal-button-unhide");
+const hideModalButton = document.querySelector(".modal-button-hide");
+const modal = document.querySelector(".modal");
+
+const handleModalUnhide = () => {
+  modal.classList.remove("modal-dispay-none");
+};
+
+const handleModalHide = () => {
+  modal.classList.add("modal-dispay-none");
+};
+
+unHideModalButton.addEventListener("click", handleModalUnhide);
+hideModalButton.addEventListener("click", handleModalHide);
