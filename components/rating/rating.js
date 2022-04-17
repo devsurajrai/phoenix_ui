cc6e0adf188719258e15f053446845fa0b92d12c
Suraj Rai <surajrai@Surajs-MacBook-Pro.local>
2022-04-17 06:48:07 +0530

    add: addded js link

--- components/rating/rating.js
+++ components/rating/rating.js
@@ -1,7 +1,6 @@
 const ratingStars = document.querySelectorAll(".rating");
 
 const handleRating = (idx, ratingStars) => {
-  console.log("Hello");
   // for chnaging the star color to blue to the preceeding stars to the clicked star
   for (let i = 0; i <= idx; i++) {
     if (!ratingStars[i].classList.contains('"rating-star-active"')) {
--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// RATING
+const ratingStars = document.querySelectorAll(".rating");
+
+const handleRating = (idx, ratingStars) => {
+  console.log("Clicked");
+  // for chnaging the star color to blue to the preceeding stars to the clicked star
+  for (let i = 0; i <= idx; i++) {
+    if (!ratingStars[i].classList.contains('"rating-star-active"')) {
+      ratingStars[i].classList.add("rating-star-active");
+    }
+  }
+  //   for changing the star color to default beyond the clicked star
+  for (let i = idx + 1; i < ratingStars.length; i++) {
+    ratingStars[i].classList.remove("rating-star-active");
+  }
+};
+ratingStars.forEach((star, idx) => {
+  star.addEventListener("click", () => handleRating(idx, ratingStars));
+});
+
 // MODAL
 const unHideModalButton = document.querySelector(".modal-button-unhide");
 const hideModalButton = document.querySelector(".modal-button-hide");
@@ -13,22 +33,3 @@ const handleModalHide = () => {
 
 unHideModalButton.addEventListener("click", handleModalUnhide);
 hideModalButton.addEventListener("click", handleModalHide);
-
-// RATING
-const ratingStars = document.querySelectorAll(".rating");
-
-const handleRating = (idx, ratingStars) => {
-  // for chnaging the star color to blue to the preceeding stars to the clicked star
-  for (let i = 0; i <= idx; i++) {
-    if (!ratingStars[i].classList.contains('"rating-star-active"')) {
-      ratingStars[i].classList.add("rating-star-active");
-    }
-  }
-  //   for changing the star color to default beyond the clicked star
-  for (let i = idx + 1; i < ratingStars.length; i++) {
-    ratingStars[i].classList.remove("rating-star-active");
-  }
-};
-ratingStars.forEach((star, idx) => {
-  star.addEventListener("click", () => handleRating(idx, ratingStars));
-});
